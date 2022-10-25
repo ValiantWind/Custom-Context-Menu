e29ec26dc57934d0cda7f88413c3b93a3592ef1c
Pavan B <83314733+ValiantWind@users.noreply.github.com>
2022-10-25 16:26:24 +0000

    Tried and failed to make it mobile compatible

--- script.js
+++ script.js
@@ -3,7 +3,38 @@ const scope = document.querySelector("body");
 const reload = document.getElementById("reloadPage");
 const printElement = document.getElementById("print");
 const savePage = document.getElementById("savePage");
-const shareButton = document.getElementById("share");
+const shareContextMenuButton = document.getElementById("shareContextMenuButton");
+const shareButton = document.getElementById("shareButton");
+
+// The Ripple Animation for the Button
+
+const buttons = document.getElementsByTagName("button");
+
+function createRipple(event) {
+  const button = event.currentTarget;
+
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+}
+
+// Loop through all the buttons on the website and apply the ripple effect to each one
+for (const button of buttons) {
+  button.addEventListener("click", createRipple);
+}
 
 const normalizePozition = (mouseX, mouseY) => {
         // ? compute what is the mouse position relative to the container element (scope)
@@ -75,10 +106,18 @@ printElement.addEventListener("click", (event) => {
 	window.print();
 })
 
+shareContextMenuButton.addEventListener("click", (event) => {
+	navigator.share({
+		title: document.title,
+    text: "Check out this really cool project that @ValiantWind made!!1!111!!",
+    url: window.location.href
+	})
+});
+
 shareButton.addEventListener("click", (event) => {
 	navigator.share({
 		title: document.title,
     text: "Check out this really cool project that @ValiantWind made!!1!111!!",
     url: window.location.href
 	})
-})
+});
